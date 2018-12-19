a2f28d0b9718fb199fc36e2d4e165a297189538f
marekswan07 <marekswan07@hotmail.com>
2018-12-18 23:01:47 -0700

    Everything is working!!

---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Napoelon Dynamite",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Comedy",
+                        Rating = "PG",
+                        Price = 7.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Singles Ward 2 ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Romantic Comedy",
+                        Rating = "PG",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Baptists at Our Barbecue",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Romantic Comedy",
+                        Rating = "PG",
+                        Price = 9.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = " Saints and Soldiers",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "War",
+                        Rating = "M",
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
